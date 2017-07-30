042f41cd097872ac75d17039e7e6ac61125f7a9f
Marat <valiev.m@gmail.com>
2017-07-29 23:43:11 -0400

    version-based packages info (vs. package-based)

--- pypi/management/commands/pypi_packages_info.py
+++ pypi/management/commands/pypi_packages_info.py
@@ -1,47 +1,62 @@
 
 import csv
 import argparse
-import shutil
+import logging
+import multiprocessing
 
 from django.core.management.base import BaseCommand
 
+import settings
 from pypi import utils
+from common import decorators
+from common import threadpool
+
+logging.basicConfig()
+logger = logging.getLogger('ghd.scraper')
+CPU_COUNT = multiprocessing.cpu_count()
 
 
 class Command(BaseCommand):
-    help = 'Produce a CSV list of PyPi packages including their dependencies,' \
-           'Github repositories, Google Groups and LOC size'
+    help = 'TBD'
 
     def add_arguments(self, parser):
         parser.add_argument('-o', '--output', default="-",
                             type=argparse.FileType('w'),
                             help='Output filename, "-" or skip for stdout')
+        parser.add_argument('-w', '--workers', default=1, type=int,
+                            help='Number of workers to use (1 by default)')
 
     def handle(self, *args, **options):
-        writer = csv.writer(options['output'])
-        writer.writerow([
-            'name', 'github_url', 'google_group', 'dependencies'
-        ])
+        loglevel = 40 - 10*options['verbosity']
+        logger.setLevel(loglevel)
 
-        # TODO: workers pool
+        writer = csv.DictWriter(
+            options['output'],
+            fieldnames=['name', 'version', 'date', 'dependencies', 'size'])
+        save_path = decorators.mkdir(settings.DATASET_PATH, 'pypi')
+
+        def target(p):
+            return [{
+                'name': p.name,
+                'version': label,
+                'date': date,
+                'dependencies': ",".join(p.dependencies(label)),
+                'size': p.size(label)
+            } for label, date in p.releases()]
+
+        def callback(rows):
+            writer.writerows(rows)
+            options['output'].flush()
+
+        workers = min(max(options['workers'], 1), CPU_COUNT)
+        tp = threadpool.ThreadPool(workers)
+
         for package_name in utils.list_packages():
             try:
-                p = utils.Package(package_name)
+                p = utils.Package(package_name, save_path=save_path)
             except utils.PackageDoesNotExist:
                 # some deleted packages aren't removed from the list
                 continue
 
-            writer.writerow([
-                package_name,
-                p.github_url,
-                p.google_group,
-                ",".join(p.dependencies)
-                # p.size  # takes a lot of time to process
-            ])
-            options['output'].flush()
-            if getattr(p, '_pkgdir', None):
-                try:
-                    # some archives have broken permissions, so won't delete
-                    shutil.rmtree(p._pkgdir)
-                except:
-                    pass
+            logger.debug("Processing %s", package_name)
+            tp.submit(target, p, callback=callback)
