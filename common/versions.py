f9c15d63532b20aca8c596cc478b389ff3b9a8f8
Marat <user2589@users.noreply.github.com>
2018-01-26 23:46:14 -0500

    common.versions tests

--- common/versions.py
+++ common/versions.py
@@ -2,11 +2,34 @@
 import re
 
 
+def is_alpha(version):
+    """ Check whether the provided version is not a stable release
+    This method is looking if version matches to digits and dots only
+    :param version: str, version string
+    :return: bool
+
+    >>> is_alpha("1.0.0")
+    False
+    >>> is_alpha("1.0rc1")
+    True
+    >>> is_alpha("0.0.0.0")
+    False
+    """
+    return not re.match("^\d+(\.\d+)*$", version.strip())
+
+
 def parse(version):
     # type: (str) -> list
     """ Transform version string into comparable list
     :param version: version string, e.g. 0.11.23rc1
     :return: list of version chunks, e.g. [0, 11, 23, 'rc1']
+
+    >>> parse("1")
+    [1]
+    >>> parse("0.0.1")
+    [0, 0, 1]
+    >>> parse("0.11.23rc1")
+    [0, 11, 23, 'rc1']
     """
     chunks = []
     for chunk in re.findall(r"(\d+|[A-Za-z]\w*)", version):
@@ -22,15 +45,15 @@ def compare(ver1, ver2):
     # type: (str, str) -> int
     """Compares two version string, returning {-1|0|1} just as cmp().
     (-1: ver1 < ver2, 0: ver1==ver2, 1: ver1 > ver2)
-    >>> compare_versions("0.1.1", "0.1.2")
+    >>> compare("0.1.1", "0.1.2")
     -1
-    >>> compare_versions("0.1.2", "0.1.1")
+    >>> compare("0.1.2", "0.1.1")
     1
-    >>> compare_versions("0.1", "0.1.1")
+    >>> compare("0.1", "0.1.1")
     0
-    >>> compare_versions("0.1.1rc1", "0.1.1a")
+    >>> compare("0.1.1rc1", "0.1.1a")
     1
-    >>> compare_versions("0.1.1rc1", "0.1.1")
+    >>> compare("0.1.1rc1", "0.1.1")
     -1
     """
     chunks1 = parse(str(ver1))
--- fabfile.py
+++ fabfile.py
@@ -13,8 +13,9 @@ fab.env.hosts = settings.DEPLOY_HOSTS
 def test():
     with fab.settings(warn_only=True):
         fab.local("python -m unittest common.test")
-        fab.local("python -m doctest -v common/utils.py")
-        fab.local("python -m doctest -v pypi/utils.py")
+        fab.local("python -m doctest common/utils.py")
+        fab.local("python -m doctest common/versions.py")
+        fab.local("python -m doctest pypi/utils.py")
         fab.local("python -m unittest scraper.test")
 
 
